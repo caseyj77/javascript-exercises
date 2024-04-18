afe0b7d6547f06ab2832f6e68e514158a1aecd93
Zorishimo <caseyj77@gmail.com>
2024-04-18 12:37:00 -0500

    competed js ex no 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArray, ...args) {
+    const newArray = []  //creates a container to hold the results in an array
+    userArray.forEach(items => {
+        if (!args.includes(items)) {
+            newArray.push(items)
+        }               // takes the userArray and seraches for the items in args and then pushes those items in newArray. However since ! is included it will only add the items not in the args
+                        // this effectivly removes the items from the array. 
+                        // I had to look this one up on youtube but learned how to use ...args, forEach, included and push.
+        
+    });
+    return newArray
 };
 
 // Do not edit below this line
